a572fee7bb29e8e24d668b972a89fe678b4d74e0
Daniek <info@daniek.io>
2023-01-02 15:15:40 +0100

    add

---
+++ touchtyping.js
@@ -0,0 +1,31 @@
+// To import readlineSync module
+const readlineSync = require('readline-sync');
+
+function practiceTouchTyping(length) {
+    // Generate a random string to type
+    const correctString = generateRandomString(length);
+    console.log(`Type the following string: ${correctString}`);
+
+    // Get user's input
+    const userInput = readlineSync.question('Enter the string: ');
+
+    // Check if the user's input is correct
+    if (userInput === correctString) {
+        console.log('Correct!')
+    } else {
+        console.log(`Incorrect. The correct string was: ${correctString}`)
+    }
+}
+
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz;';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+practiceTouchTyping(10);
